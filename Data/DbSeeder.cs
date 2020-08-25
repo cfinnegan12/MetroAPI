8423bc8d4abd65ce396285746b6b42d15a2483d5
Ciaran Finnegan <cfinnegan12@qub.ac.uk>
2020-08-25 16:11:36 +0100

    Journey & Stop Seeding Implemented

--- Data/DbSeeder.cs
+++ Data/DbSeeder.cs
@@ -2,6 +2,8 @@
 using ATCOcif;
 using Microsoft.EntityFrameworkCore.Internal;
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MetroAPI.Data
 {
@@ -15,6 +17,7 @@ namespace MetroAPI.Data
 
             if (!context.Locations.Any()) SeedLocations(context);
             if (!context.Routes.Any()) SeedRoutes(context);
+            if (!context.Journeys.Any()) SeedJourneys(context);
 
         }
 
@@ -58,11 +61,12 @@ namespace MetroAPI.Data
                 {
                     Console.WriteLine("Seeding routes: " + i + "/" + _parser.Routes.Count);
 
-                    context.Routes.Add(new Route { 
+                    context.Routes.Add(new Route
+                    {
                         RouteNumber = route.Route,
                         Description = route.Description,
-                        Direction = (route.Direction == Direction.Inbound) ? "Inbound" : "Outbound"
-                    });
+                        Direction = route.Direction.ToString("G")
+                    }); ;
                     i++;
                 }
                 context.SaveChanges();
@@ -76,6 +80,73 @@ namespace MetroAPI.Data
 
             Console.WriteLine("Routes successfully seeded");
             return true;
+        }
+
+        private static bool SeedJourneys(MetroDBContext context)
+        {
+            int i = 0;
+            try
+            {
+                foreach (JourneyRecord journey in _parser.Journeys)
+                {
+                    Console.WriteLine("Seeding journeys: " + i + "/" + _parser.Journeys.Count);
+                    Route journeyRoute = context.Routes
+                        .Where(r => r.RouteNumber == journey.Header.Route && r.Direction == (journey.Header.Direction.ToString("G")))
+                        .FirstOrDefault();
+
+                    context.Journeys.Add(new Journey
+                    {
+                        Route = journeyRoute,
+                        Monday = journey.Header.OperationDays[0],
+                        Tuesday = journey.Header.OperationDays[1],
+                        Wednesday = journey.Header.OperationDays[2],
+                        Thursday = journey.Header.OperationDays[3],
+                        Friday = journey.Header.OperationDays[4],
+                        Saturday = journey.Header.OperationDays[5],
+                        Sunday = journey.Header.OperationDays[6],
+                        BankHolidays = journey.Header.BankHolidays,
+                        Stops = StopsFromJourney(journey, context)
+                    });
+ 
+                    i++;
+                }
+                context.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Journey seeding failed: \n" + e.Message);
+                return false;
+            }
+
+            Console.WriteLine("Journey successfully seeded");
+            return true;
+        }
+
+        private static List<Stop> StopsFromJourney(JourneyRecord journeyRecord, MetroDBContext context)
+        {
+            List<Stop> result = new List<Stop>();
+            result.Add(StopFromRecord(context, journeyRecord.Origin, 'O'));
+
+            foreach (StopRecord record in journeyRecord.IntermediateRecords)
+                result.Add(StopFromRecord(context, record, 'I'));
+
+            result.Add(StopFromRecord(context, journeyRecord.Destination, 'D'));
+
+            return result;
+        }
+
+        private static Stop StopFromRecord(MetroDBContext context, StopRecord record, char type = 'I')
+        {
+            Location location = context.Locations
+                .Where(l => l.ShortForm == record.Location).FirstOrDefault();
+
+            return new Stop
+            {
+                Location = location,
+                Time = new TimeSpan(0, record.Time.Hours, record.Time.Minutes, record.Time.Seconds),
+                StopType = type
+            };
         }
     }
 }
